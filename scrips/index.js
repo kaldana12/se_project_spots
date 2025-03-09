37b71b3eaec88afa195e8d6183267ef7536e82e4
KAY <karen.aldana012@gmail.com>
2025-03-09 09:58:42 -0400

    stage 4 part2

---
+++ scrips/index.js
@@ -0,0 +1,22 @@
+const initialCards = {
+    {name: "Woman",
+    link: "https://unsplash.com/photos/a-woman-standing-in-front-of-a-mirror-brushing-her-teeth-vuwDHNmwvHQ"},
+
+    {name: "Window",
+    link: "https://unsplash.com/photos/a-woman-standing-in-front-of-a-window-FXNvIe2XrZk"},
+
+   {name: "Building",
+    link: "https://unsplash.com/photos/a-black-and-white-photo-of-a-building-with-graffiti-on-it-cDzAdwff-8U"},
+    
+   {name: "Camera",
+    link: "https://unsplash.com/photos/a-person-holding-a-camera-up-in-the-air-yEhrHJ3vDEI"},
+   
+    {name: "Shadow",
+    link: "https://unsplash.com/photos/a-person-sitting-on-a-bench-in-the-dark-00rKfDokg_Y"},
+
+    {name: "Snow",
+    link: "https://unsplash.com/photos/a-view-of-a-dome-on-top-of-a-building-in-the-snow-vfrdWR2Shmw"},
+};
+
+
+console.log(initialCards);
